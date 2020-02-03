3428fee0b07253c06cb5767f2177fb228333301e
KaluginDV <interrupted-man@yandex.ru>
2020-02-04 01:01:22 +0800

    Home work 2 - finished

--- hw2/script.js
+++ hw2/script.js
@@ -1,43 +1,42 @@
-'use strict';
 
-/*2) В файле скрипта создать 2 переменные (money и time), 
-которые будут получать данные от пользователя:*/
-var money = prompt("Ваш бюджет на месяц?", 0);
-var time = prompt("Введите дату в формате YYYY-MM-DD");
+var money   = +prompt("Ваш бюджет на месяц?", 0), 
+    time    = prompt("Введите дату в формате YYYY-MM-DD");
 
-/*3) Создать объект appData, который будет содержать такие данные:*/
 let appData = {
-    money: money,
-    time: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
+    budget:             money,
+    timeData:           time,
+    expenses:           {},
+    optionalExpenses:   {},
+    income:             [],
+    savings:            false
 };
 
-console.log(appData);
+for (let i = 0; i < 2; i++){
+    var item = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        cost = +prompt("Во сколько обойдется?", '');
 
-/*4) Задать пользователю по 2 раза вопросы:*/
-var question1 = "Введите обязательную статью расходов в этом месяце";
-var question2 = "Во сколько обойдется?";
-var item1 = prompt(question1);
-var cost1 = prompt(question2);
-appData.expenses[item1] = cost1;
-var item2 = prompt(question1);
-var cost2 = prompt(question2);
-appData.expenses[item2] = cost2;
+    if ((typeof(item)) === 'string' 
+        && (typeof(item)) != null
+        && (typeof(cost)) != null
+        && item != '' && cost != ''
+        && item.length < 50){
+        appData.expenses[item] = cost;
+    } else {
+        i--;
+        continue;
+    }
+}
 
-/*5) Вывести на экран пользователя бюджет на 1 день 
-(брать месяц за 30 дней, использовать alert)*/
-alert("Бюджет на 1 день равен: " + money/30);
+appData.moneyPerDay = appData.budget / 30;
 
-/*6) Проверить, чтобы все работало без ошибок в консоли*/
-/*Проверено*/
+alert("Бюджет на 1 день равен: " + appData.moneyPerDay);
 
-/*Вопросы к этому заданию
-Сколько типов данных существует в JS?
-7 простых, 5 специальных и обычные типы.
-Как вывести информацию в консоль?
-console.log("message");
-Какая функция операторов || и &&?
-|| - оператор "И", && - оператор "ИЛИ". Используются для проверки условия.*/
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уроыень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка")
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка")
+} else {
+    console.log("Произошла ошибка");
+}
