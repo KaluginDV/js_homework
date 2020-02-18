b707961a538208fb5ad4d912016d9dd7c7e0a822
KaluginDV <interrupted-man@yandex.ru>
2020-02-19 00:35:09 +0800

    To be continued...

---
+++ hw9/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('info-header'),
+        tabContent = document.querySelectorAll('info-tabcontent');
+
+});
