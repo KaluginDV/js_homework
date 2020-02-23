e70129f788c85e59a833dc290200934b6a8cafe5
KaluginDV <interrupted-man@yandex.ru>
2020-02-23 22:04:57 +0800

    HW13 part 1

--- hw9/js/script.js
+++ hw9/js/script.js
@@ -110,4 +110,52 @@ window.addEventListener('DOMContentLoaded', function(){
         });
     }
 
+       // Form
+       let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let mainForm = document.querySelector('.main-form'),
+        input = mainForm.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+    mainForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        mainForm.appendChild(statusMessage);
+
+        //request
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(mainForm);
+
+        let obj = {};
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            }  
+            else if(request.readyState === 4 && request.status == 200){
+                statusMessage.innerHTML = message.success;
+            }
+            else{
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i ++){
+            input[i].value = '';
+        }
+    });
+
 });
