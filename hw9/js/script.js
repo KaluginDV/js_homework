876ad43fca2480faab9ffba9c16f77d1f4bc52fc
KaluginDV <interrupted-man@yandex.ru>
2020-02-26 16:59:39 +0800

    Complete HW14

--- hw9/js/script.js
+++ hw9/js/script.js
@@ -126,31 +126,44 @@ window.addEventListener('DOMContentLoaded', function(){
         event.preventDefault();
         mainForm.appendChild(statusMessage);
 
-        //request 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        //request
+        function catchMessage(){
 
-        let formData = new FormData(mainForm);
+            let promise = new Promise(function(resolve, reject){
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        
+                let formData = new FormData(mainForm);
+        
+                let obj = {};
+                formData.forEach(function(value, key){
+                    obj[key] = value;
+                });
+                let json = JSON.stringify(obj);
+        
+                request.send(json);
+                
+                request.onload = function(){
+                    if (request.readyState < 4){
+                        resolve(message.loading);
+                    }  
+                    else if(request.readyState === 4 && request.status == 200){
+                        resolve(message.success);
+                    }
+                    else{
+                        reject(message.failure);
+                    }  
+                };
+            });
 
-        let obj = {};
-        formData.forEach(function(value, key){
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
+            return promise;
+        }
 
-        request.send(json);
-
-        request.addEventListener('readystatechange', function(){
-            if (request.readyState < 4){
-                statusMessage.innerHTML = message.loading;
-            }  
-            else if(request.readyState === 4 && request.status == 200){
-                statusMessage.innerHTML = message.success;
-            }
-            else{
-                statusMessage.innerHTML = message.failure;
-            }
+        catchMessage().then(message => {
+            statusMessage.innerHTML = message;    
+        }).then(message => {
+            statusMessage.innerHTML = message;     
         });
 
         for (let i = 0; i < input.length; i ++){
